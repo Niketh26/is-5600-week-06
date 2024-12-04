c381a50ba484763c0c9bf0afe92952e82bddcc85
Niketh26 <nikethreddy.geereddy@slu.edu>
2024-12-04 04:50:59 +0000

    final 06

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import productData from './data/full-products';
+/*import productData from './data/full-products';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello There.</h1>
+    </div>
+  );
+}
+
+export default App;
+*/
+
+
+import Header from './components/Header';
+import CardList from './components/CardList';
+import { Route, Routes} from 'react-router-dom';
+import productData from './data/full-products';
+import SingleView from './components/SingleView';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+          <Route path="/" element={<CardList data={productData} />} />
+          <Route path="/product/:id" element={<SingleView data={productData} />} />
+        </Routes>
     </div>
   );
 }
